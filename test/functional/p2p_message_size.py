398f8b6f36334d26509a6e8978fd81c8979ef438
Zako <65051353+Jegertabt@users.noreply.github.com>
2026-08-01 18:09:06 -0400

    test: construct p2p message-size peers before network thread

--- test/functional/p2p_message_size.py
+++ test/functional/p2p_message_size.py
@@ -56,22 +56,23 @@ class MessageSizeTest(BitcoinTestFramework):
         assert 178723 < MAX_PROTOCOL_MESSAGE_LENGTH
         assert 2000 * 4096 < MAX_PROTOCOL_MESSAGE_LENGTH
 
-        self.log.info("Exact 8 MiB claimed size stays under the receive gate")
         under = self.nodes[0].add_p2p_connection(MessageSizeP2P())
+        over = self.nodes[0].add_p2p_connection(MessageSizeP2P())
+        ok = self.nodes[0].add_p2p_connection(MessageSizeP2P())
         network_thread_start()
+
+        self.log.info("Exact 8 MiB claimed size stays under the receive gate")
         under.wait_for_verack()
         under.send_claimed_size("ping", MAX_PROTOCOL_MESSAGE_LENGTH)
         time.sleep(1)
         assert under.connected
 
         self.log.info("8 MiB + 1 claimed size disconnects the peer")
-        over = self.nodes[0].add_p2p_connection(MessageSizeP2P())
         over.wait_for_verack()
         over.send_claimed_size("ping", MAX_PROTOCOL_MESSAGE_LENGTH + 1)
         wait_until(lambda: not over.connected, timeout=10)
 
         self.log.info("Ordinary small P2P traffic still works under 8 MiB")
-        ok = self.nodes[0].add_p2p_connection(MessageSizeP2P())
         ok.wait_for_verack()
         ok.send_message(msg_ping())
         time.sleep(1)
